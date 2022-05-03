a2b2b4c11b2c41f8fb95b71b71a94732b601409c
David Webb <dave@davidwebb.me.uk>
2022-05-03 18:17:40 +0100

    :sparkles: Update e-paper display 1. Add module for updating e-paper display 2. Make __main__.py use new module

--- nimbus/__main__.py
+++ nimbus/__main__.py
@@ -1,18 +1,37 @@
 """Display bus times on an e-ink screen"""
 
 from nimbus import nextbus
+from nimbus import epaper
+from datetime import datetime
+import math
 
 def main():
     """Entrypoint function"""
 
-    #bus_stop_id = 'oxfadgpm'
-    bus_stop_id = 'oxfadgdw'
-    (refresh_time, buses) = nextbus.extract_bus_information(bus_stop_id)
+    #bus_stop_id = 'oxfadgdw'
+    bus_stop_id = 'oxfadgpm'
+    bus_stop_name = 'Raleigh Park Road'
 
-    print(refresh_time)
+    (refresh_time, raw_buses) = nextbus.extract_bus_information(bus_stop_id)
 
-    for bus in buses:
-        print(bus)
+    buses = []
+
+    for (bus, destination, due) in raw_buses[:3]:
+        seconds = (due - refresh_time).total_seconds()
+        print(refresh_time,due,seconds)
+
+        if seconds == 0:
+            due = 'due'
+        elif seconds < 61:
+            due = '1 min'
+        else:
+            minutes = math.ceil(seconds / 60)
+            due = '{} mins'.format(minutes)
+
+        buses.append((bus, destination, due))
+
+    print(buses)
+    epaper.display(bus_stop_name, buses, refresh_time.strftime('Last updated %H:%M'))
 
 if __name__ == '__main__':
     main()
