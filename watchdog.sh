0356ef58f9ad67ca3b7deaf0bc308bb2e44931e5
David Webb <dwebb@tripadvisor.com>
2022-05-21 13:57:46 +0100

    :bug: Make watchdog log append

--- watchdog.sh
+++ watchdog.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if test -z $(find ${HEARTBEAT_FILE} -mmin -${AGE_MINUTES})
 then
-    echo $(date -Iminutes) Watchdog rebooting system >${SCRIPT_DIR}/watchdog.log
+    echo $(date -Iminutes) Watchdog rebooting system >>${SCRIPT_DIR}/watchdog.log
     sudo reboot
 fi
