5cb4388f4d20686b130854cfcfcc5b00057c7882
David Webb <dave@davidwebb.me.uk>
2022-05-21 13:45:05 +0100

    :sparkles: Add heartbeating

---
+++ watchdog.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HEARTBEAT_FILE=/tmp/heartbeat
+AGE_MINUTES=3
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if test -z $(find ${HEARTBEAT_FILE} -mmin -${AGE_MINUTES})
+then
+    echo $(date -Iminutes) Watchdog rebooting system >${SCRIPT_DIR}/watchdog.log
+    sudo reboot
+fi
